62d6a17bbe14aed172c5d52b619f86b2e652e203
qwantanius <borshbrawl@gmail.com>
2021-06-24 10:59:24 +0800

    Clear

--- Download.bash
+++
@@ -1,5 +0,0 @@
-echo "package master - update"
-sudo apt-get update && sudo apt-get upgrade -y
-echo "start downloading tools"
-sudo apt-get install git php python ngrok snapd
-sudo snap install ngrok
